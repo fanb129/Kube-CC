9a83f30554600097f8d051faaf539b62c9cb81e8
fanb <1916861581@qq.com>
2023-06-26 23:39:32 +0800

    [add] 将一个router文件分解为多个

--- routers/router.go
+++ routers/router.go
@@ -1,17 +1,8 @@
 package routers
 
 import (
-	"Kube-CC/api/v1/deploy"
-	"Kube-CC/api/v1/hadoop"
-	"Kube-CC/api/v1/linux"
-	"Kube-CC/api/v1/login"
-	"Kube-CC/api/v1/namespace"
 	"Kube-CC/api/v1/node"
 	"Kube-CC/api/v1/pod"
-	"Kube-CC/api/v1/spark"
-	"Kube-CC/api/v1/svc"
-	"Kube-CC/api/v1/user"
-	"Kube-CC/api/v1/yaml"
 	"Kube-CC/conf"
 	"Kube-CC/middleware"
 	"github.com/gin-gonic/gin"
@@ -27,107 +18,43 @@ func InitRouter() *gin.Engine {
 
 	r.GET("/api/node/ssh", node.WsSsh)
 	r.GET("/api/pod/ssh", pod.Ssh)
+
 	apiRouter := r.Group("/api")
-	{
-		apiRouter.POST("/login", login.Login) // 登陆路由
-		apiRouter.POST("/checkPass", login.CheckPass)
-		apiRouter.GET("/logout", login.Logout)      // 登出路由
-		apiRouter.POST("/register", login.Register) // 注册路由
-		apiRouter.GET("/Captcha", login.GetCaptcha) // 验证码
-	}
+	// 登陆路由
+	loginRouter(apiRouter)
 
 	// 需要鉴权
 	auth := apiRouter.Group("", middleware.JWTToken())
 
 	// yaml路由
-	yamlRouter := auth.Group("/yaml")
-	{
-		yamlRouter.POST("/apply", yaml.Apply)
-		yamlRouter.POST("/create", yaml.Create)
-	}
+	yamlRouter(auth)
 
 	//用户路由
-	userRouter := auth.Group("/user")
-	{
-		userRouter.GET("/info", user.Info)
-		userRouter.GET("/:page", user.Index)                                    // 浏览用户信息
-		userRouter.GET("/delete/:id", middleware.Is2Role(), user.Delete)        // 删除用户
-		userRouter.POST("/edit/:id", middleware.Is2Role(), user.Edit)           // 授权用户
-		userRouter.POST("/resetpass/:id", middleware.Is2Role(), user.ResetPass) // 重置密码
-		userRouter.POST("/update/:id", user.Update)                             // 更新用户信息
-	}
+	userRouter(auth)
 
 	// node路由
-	nodeRouter := auth.Group("/node")
-	{
-		nodeRouter.GET("", node.Index) // 浏览所有node
-		nodeRouter.GET("/delete/:node", middleware.Is3Role(), node.Delete)
-		nodeRouter.POST("/add", middleware.Is3Role(), node.Add)
-	}
+	nodeRouter(auth)
 
 	// namespace路由
-	nsRouter := auth.Group("/ns")
-	{
-		nsRouter.GET("", namespace.Index)                                // 浏览所有namespace
-		nsRouter.GET("/delete/:ns", namespace.Delete)                    // 删除指定namespace
-		nsRouter.POST("/add", middleware.Is2Role(), namespace.Add)       // 添加namespace
-		nsRouter.POST("/update", middleware.Is2Role(), namespace.Update) // 更新uid
-	}
+	nsRouter(auth)
 
 	// deploy路由
-	deployRouter := auth.Group("/deploy")
-	{
-		deployRouter.GET("", deploy.Index)
-		deployRouter.GET("/delete", deploy.Delete)
-		deployRouter.GET("/info", deploy.Info)
-		//deployRouter.POST("/add", deploy.Add) // 通过表单添加deploy
-	}
+	deployRouter(auth)
 
-	serviceRouter := auth.Group("/service")
-	{
-		serviceRouter.GET("", svc.Index)
-		serviceRouter.GET("/delete", svc.Delete)
-		serviceRouter.GET("/info", svc.Info)
-	}
+	// service路由
+	serviceRouter(auth)
+
 	// pod路由
-	podRouter := auth.Group("/pod")
-	{
-		podRouter.GET("", pod.Index) // 浏览指定namespace的pod
-		podRouter.GET("/delete", pod.Delete)
-		podRouter.GET("/info", pod.Info)
-	}
+	podRouter(auth)
 
 	// spark路由
-	sparkRouter := auth.Group("/spark")
-	{
-		//sparkRouter.POST("/add", middleware.Is2Role(), spark.Add) // 新建spark集群
-		// 批量添加
-		sparkRouter.POST("/add", middleware.Is2Role(), spark.BatchAdd) // 新建spark集群
-		sparkRouter.GET("/delete/:name", spark.Delete)
-		sparkRouter.GET("", spark.Index)
-		sparkRouter.POST("/update", middleware.Is2Role(), spark.Update)
-	}
+	sparkRouter(auth)
 
-	hadoopRouter := auth.Group("/hadoop")
-	{
-		//hadoopRouter.POST("/add", middleware.Is2Role(), hadoop.Add)
-		// 批量添加
-		hadoopRouter.POST("/add", middleware.Is2Role(), hadoop.BatchAdd)
-		hadoopRouter.GET("/delete/:name", hadoop.Delete)
-		hadoopRouter.GET("", hadoop.Index)
-		hadoopRouter.POST("/update", middleware.Is2Role(), hadoop.Update)
-	}
+	// hadoop路由
+	hadoopRouter(auth)
 
 	// linux路由
-	linuxRouter := auth.Group("/linux")
-	{
-		linuxRouter.GET("", linux.Index)
-		linuxRouter.GET("delete/:name", linux.Delete)
-		//linuxRouter.POST("/add", middleware.Is2Role(), linux.Add)
-		// 批量添加
-		linuxRouter.POST("/add", middleware.Is2Role(), linux.BatchAdd)
-		linuxRouter.POST("/update", middleware.Is2Role(), linux.Update)
-	}
+	linuxRouter(auth)
 
 	return r
 }
---
+++ routers/yaml.go
@@ -0,0 +1,14 @@
+package routers
+
+import (
+	"Kube-CC/api/v1/yaml"
+	"github.com/gin-gonic/gin"
+)
+
+func yamlRouter(router *gin.RouterGroup) {
+	yamlRouters := router.Group("/yaml")
+	{
+		yamlRouters.POST("/apply", yaml.Apply)
+		yamlRouters.POST("/create", yaml.Create)
+	}
+}
