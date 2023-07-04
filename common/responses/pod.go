88e51c48ada4cf80956716c7d9e8718d8eac39cc
fanb <1916861581@qq.com>
2023-07-04 23:28:43 +0800

    [update] 重大更新!!!向用户屏蔽k8s的概念，代码结构优化

--- common/responses/pod.go
+++ common/responses/pod.go
@@ -1,24 +1,8 @@
 package responses
 
-import corev1 "k8s.io/api/core/v1"
-
-type Pod struct {
-	Name              string                   `json:"name"`
-	Namespace         string                   `json:"namespace"`
-	CreatedAt         string                   `json:"created_at"`
-	Phase             corev1.PodPhase          `json:"phase"`
-	NodeIp            string                   `json:"node_ip"`
-	ContainerStatuses []corev1.ContainerStatus `json:"container_statuses"`
-	Uid               string                   `json:"u_id"`
-}
-
-type PodListResponse struct {
-	Response
-	Length  int   `json:"length"`
-	PodList []Pod `json:"pod_list"`
-}
-
-type PodInfo struct {
-	Response
-	Info corev1.Pod `json:"info"`
+type DeployPod struct {
+	Name   string `json:"name"`
+	Phase  string `json:"phase"`
+	HostIP string `json:"host_ip"`
+	PodIP  string `json:"pod_ip"`
 }
